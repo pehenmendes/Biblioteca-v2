81fc0a834e51cad647f39610ed06820c801278e3
mendes <pehenmendes@gmail.com>
2024-10-15 10:41:39 -0300

    images

--- src/components/Pesquisa/dadosPesquisa.js
+++ src/components/Pesquisa/dadosPesquisa.js
@@ -1,4 +1,4 @@
-import livro from '../../imagens/livro.png'
+import livro from '../../images/livro.png'
 
 export const livros = [
     {nome: 'Liderança em Design', id: 1, src:livro},
--- src/components/UltimosLancamentos/dadosUltimosLancamentos.js
+++ src/components/UltimosLancamentos/dadosUltimosLancamentos.js
@@ -1,6 +1,6 @@
-import ProgressiveWebApps from '../../imagens/Progressive_Web_Apps.jpg'
-import Sass from '../../imagens/Sass.png'
-import TurbineSeuCSS from '../../imagens/Turbine_seu_CSS.png'
+import ProgressiveWebApps from '../../images/Progressive_Web_Apps.jpg'
+import Sass from '../../images/Sass.png'
+import TurbineSeuCSS from '../../images/Turbine_seu_CSS.png'
 
 export const livros = [
     {nome: 'Progressive Web Apps', id: 9, src: ProgressiveWebApps},
